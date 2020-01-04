afeef7726685a6fda6f29820f03b2a5f144fda7a
picode98 <saaman1377@gmail.com>
2020-01-04 18:59:29 -0500

    Upload existing project files

---
+++ Test1/commands/drive_immediate.py
@@ -0,0 +1,12 @@
+from wpilib.command import Command
+
+class Drive_Immediate(Command):
+    def __init__(self, init_drive_subsystem, Y, X):
+        super().__init__("drive_immediate", timeout=0.05, subsystem=init_drive_subsystem)
+        self.drive_subsystem = init_drive_subsystem
+        self.y_speed = Y
+        self.x_speed = X
+
+    def execute(self):
+        self.drive_subsystem.arcade_drive(self.y_speed, self.x_speed)
+
---
+++ Test1/commands/drive_reserve.py
@@ -0,0 +1,11 @@
+from wpilib.command import Command
+
+
+class Drive_Reserve(Command):
+    def __init__(self, drive_subsystem):
+        super().__init__("drive_reserve", subsystem=drive_subsystem)
+        self.drive_subsystem = drive_subsystem
+
+    def execute(self):
+        self.drive_subsystem.arcade_drive(0,0)
+
